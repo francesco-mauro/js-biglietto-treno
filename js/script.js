11d2fb91914a17f726de72f91ce29544ca8bbde9
francesco-mauro <francesco.mauro180@gmail.com>
2024-03-25 16:17:49 +0100

    add: number conversion to user questions

--- js/script.js
+++ js/script.js
@@ -3,5 +3,14 @@
 
 // 1. Chiede all'utente il numero di chilometri che vuole percorrere e la sua età
 
-const chilometri = prompt("Quanti chilometri vuoi percorrere?");
-const eta = prompt("Quanti anni hai?");
+const chilometri = prompt("Quanti chilometri vuoi percorrere?"); //String
+const eta = prompt("Quanti anni hai?"); //String
+console.log(chilometri, typeof chilometri);
+console.log(eta, typeof eta);
+
+// 2. Converte le in numeri i valori "string" inseriti dall'utente
+
+const chilometriNumber = parseInt(chilometri);
+const etaNumber = parseInt(eta);
+console.log(chilometriNumber, typeof chilometriNumber);
+console.log(etaNumber, typeof etaNumber);
