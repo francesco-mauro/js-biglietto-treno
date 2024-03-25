c204e6d5b673cf7b5f2a7e8cbfdd0d56652fa845
francesco-mauro <francesco.mauro180@gmail.com>
2024-03-25 16:12:34 +0100

    add: user questions

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// alert("Ciao")
+
+
+// 1. Chiede all'utente il numero di chilometri che vuole percorrere e la sua età
+
+const chilometri = prompt("Quanti chilometri vuoi percorrere?");
+const eta = prompt("Quanti anni hai?");
